be748c933988b6c87f4b44118de90ea3416eaad1
Giovetty <kelsia20giovetty@gmail.com>
2026-05-20 14:06:05 +0100

    Added code when calculate button is clicked

--- app/src/main/java/com/example/multiplier/MainActivity.kt
+++ app/src/main/java/com/example/multiplier/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,27 +12,31 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
+    //Added variables to UI elements
+    val calculateButton= findViewById<Button>(R.id.Calculatebutton)
+    val inputNumber2= findViewById<EditText>(R.id.Inputnumber2)
+    val inputNumber1= findViewById<EditText>(R.id.InputNumber1)
+    val answerText = findViewById<TextView>(R.id.answertext)
+
+    //Created calculate function
+    fun calculate(){
+        var num1= inputNumber1.text.toString().toInt()
+        var num2= inputNumber2.text.toString().toInt()
+        var result = num1*num2
+
+        answerText.text = result.toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        //Added variables to UI elements
-        val calculateButton= findViewById<Button>(R.id.Calculatebutton)
-        val inputNumber2= findViewById<EditText>(R.id.Inputnumber2)
-        val inputNumber1= findViewById<EditText>(R.id.InputNumber1)
-        val answerText = findViewById<EditText>(R.id.answertext)
+
 
         // Added code when calculate button is clicked
         calculateButton.setOnClickListener {
-         var num1= inputNumber1.text.toString().toInt()
-
-        }
-        fun calculate(){
-            var num1= inputNumber1.text.toString().toInt()
-            var num2= inputNumber1.text.toString().toInt()
-            var result= num1*num2
-            answerText.text=result
+         calculate()
         }
 
 
